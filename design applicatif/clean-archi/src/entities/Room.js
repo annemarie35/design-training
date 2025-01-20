c4b11a3944567e0b6549235e5b53206924ae2e7f
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-20 17:54:03 +0100

    configure eslint

--- design applicatif/clean-archi/src/entities/Room.js
+++ design applicatif/clean-archi/src/entities/Room.js
@@ -1,9 +1,11 @@
-class Room {
-  constructor({ number, floor, rate }) {
-    this.floor;
-    this.number;
-    this.price;
-  }
-
-  getPriceByFloor = () => {};
-}
+// TODO Finish this part
+// eslint-disable
+// class Room {
+//   constructor({ number, floor, rate }) {
+//     this.floor;
+//     this.number;
+//     this.price;
+//   }
+//
+//   getPriceByFloor = () => {};
+// }
