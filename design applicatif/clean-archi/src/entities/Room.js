f2b82d2acf4e300f191b6832b3d7fde924ae17f0
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-27 14:59:08 +0100

    wip : add a room class in entity folder

--- design applicatif/clean-archi/src/entities/Room.js
+++ design applicatif/clean-archi/src/entities/Room.js
@@ -1,11 +1,28 @@
-// TODO Finish this part
-// eslint-disable
-// class Room {
-//   constructor({ number, floor, rate }) {
-//     this.floor;
-//     this.number;
-//     this.price;
-//   }
-//
-//   getPriceByFloor = () => {};
-// }
+const Room = class {
+  constructor({ number, floor, rate }) {
+    this.number = number;
+    this.floor = floor;
+    this.rate = rate;
+  }
+
+  getPriceByFloor({baseRate})  {
+    let newRate;
+    if (this.floor === 0) {
+      newRate = baseRate;
+    } else if (this.floor === 1) {
+      newRate = baseRate * 1.07;
+    } else if (this.floor === 2) {
+      newRate = baseRate * 1.22;
+    } else if (this.floor === 3) {
+      newRate = baseRate * 1.33;
+    }
+
+    if (newRate <= CEILING_RATE) {
+      this.rate = newRate;
+    } else {
+      this.rate = CEILING_RATE;
+    }
+  }
+}
+
+export { Room };
